1c6676a5404964594e22cae8f33f663270f634e5
Cardoso <92821038+alancardos0@users.noreply.github.com>
2023-03-28 18:15:59 -0300

    fix, component Register in the components past

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import {
 import Home from "./components/Home/Home";
 import ImageUploader from "./components/ImageUploader/ImageUploader";
 import ListAllImages from "./components/ListAllImages/ListAllImages";
-import Register from "./Register/Register";
+import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
 function App() {
   return (
